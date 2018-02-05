d1a971e30e218055d098c034fd057470ede0b760
Wontek <wontekh@gmail.com>
2018-02-05 00:07:18 +0000

    test

---
+++ Compute/ComputeWebJobsSDKQueue/Program.cs
@@ -0,0 +1,91 @@
+﻿//----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//----------------------------------------------------------------------------------
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Configuration;
+
+namespace ComputeWebJobsSDKStorageQueue
+{
+    //******************************************************************************************************
+    // This will show you how to perform common scenarios using the Microsoft Azure Queue storage service using 
+    // the Microsoft Azure WebJobs SDK. The scenarios covered include triggering a function when a new message comes
+    // on a queue, sending a message on a queue.   
+    // 
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    //
+    // TODO: Create a Storage Account through the Portal or Visual Studio and provide your [AccountName] and 
+    //       [AccountKey] in the App.Config http://go.microsoft.com/fwlink/?LinkId=325277            
+    //*****************************************************************************************************
+
+    class Program
+    {
+        static void Main()
+        {
+            if (!VerifyConfiguration())
+            {
+                Console.ReadLine();
+                return;
+            }
+
+            CreateDemoData();
+
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        private static bool VerifyConfiguration()
+        {
+            string webJobsDashboard = ConfigurationManager.ConnectionStrings["AzureWebJobsDashboard"].ConnectionString;
+            string webJobsStorage = ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString;
+
+            bool configOK = true;
+            if (string.IsNullOrWhiteSpace(webJobsDashboard) || string.IsNullOrWhiteSpace(webJobsStorage))
+            {
+                configOK = false;
+                Console.WriteLine("Please add the Azure Storage account credentials in App.config");
+
+            }
+            return configOK;
+        }
+
+        private static void CreateDemoData()
+        {
+            Console.WriteLine("Creating Demo data");
+            Console.WriteLine("Functions will store logs in the 'azure-webjobs-hosts' container in the specified Azure storage account. The functions take in a TextWriter parameter for logging.");
+           
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("initialorder");
+            CloudQueue queue2 = queueClient.GetQueueReference("initialorderproperty");
+            queue.CreateIfNotExists();
+            queue2.CreateIfNotExists();
+
+            Order person = new Order()
+            {
+                Name = "Alex",
+                OrderId = Guid.NewGuid().ToString("N").ToLower()
+            };
+
+            queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(person)));
+            queue2.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(person)));
+        }
+    }
+}
