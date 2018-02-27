45fa925784e27d88a08d2a3854dd718b1d549088
Fraz Ahmad <frazahmad@eduroam-int-dhcp-97-192-201.ucl.ac.uk>
2018-02-27 15:55:38 +0000

    config file
    connection to the database

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frazahmad
+ * Date: 27/02/2018
+ * Time: 15:22
+ */
+
+
+$con=mysqli_connect("efastdbs.mysql.database.azure.com", "efast@efastdbs", "Gv3-LST-nZU-JyP", "efast_main");
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}else{
+    echo "Hello";
+}
+
+
+mysqli_close($con);
+?>
