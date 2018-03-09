bbd31336c33d822d3719f4fb51dff8f0bfdb15da
Fraz Ahmad <frazahmad@eduroam-int-dhcp-97-30-40.ucl.ac.uk>
2018-03-09 15:16:37 +0000

    search page to bid page

--- login.php
+++ login.php
@@ -17,6 +17,7 @@
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
         -->
 
 
@@ -50,7 +51,7 @@
         </div>
         <div>
             <div>
-                <input type="text" class="form-control" id="password" name="password" placeholder="Password" required >
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required >
             </div>
             <div id="spacer" style="height: 20px"></div>
             <!-- <button type="submit" class="btn btn-primary btn-lg btn-block btn-sm">Login</button> -->
@@ -58,8 +59,112 @@
         </div>
         <div id="spacer2" style="height: 20px"></div>
         <div>
-            <button type="button" class="btn btn-primary btn-lg btn-block btn-sm" onclick="window.location='./register.html';">Sign up here</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block btn-sm" onclick="window.location='./register.php';">Sign up here</button>
         </div>
+
+
+
+        <?php include 'config.php'; ?>
+        <?php
+        if(isset($_POST["submit"]))
+        {
+
+
+
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $query = "SELECT * FROM user WHERE EMAIL = '$email' ";
+
+
+            $result = mysqli_query($conn, $query);
+
+            while($row = mysqli_fetch_assoc($result)) {
+
+                $userID = $row['ID_USER'];
+                $db_pass = $row['PASS'];
+                $first_name = $row['FNAME'];
+                $last_name = $row['LNAME'];
+                $email = $row['EMAIL'];
+                $role = $row['ID_ROLE'];
+            }
+
+            $userID = $userID;
+            $first_name =$first_name;
+            $last_name = $last_name;
+            $role = $role;
+            $db_pass =$db_pass;
+
+
+
+
+            if (!password_verify($password , $db_pass) ){
+
+                ?>
+
+
+                <div class="jumbotron">
+                    <h1 align="center">Password entered is wrong, please try again.</h1>
+                </div>
+
+                <?php
+
+                exit($status);
+
+            }
+
+            session_start();
+
+
+            // if valid
+            $_SESSION['userID'] = $userID;
+            $_SESSION['username'] = $email;
+            $_SESSION['first_name'] = $first_name;
+            $_SESSION['last_name'] = $last_name;
+            $_SESSION['role'] = $role;
+
+
+            setcookie('userID', $userID,   time() + (60 * 60 * 24 * 30));
+            setcookie('username', $email,  time() + (60 * 60 * 24 * 30));
+            setcookie('first_name', $first_name,  time() + (60 * 60 * 24 * 30));
+            setcookie('last_name', $last_name,  time() + (60 * 60 * 24 * 30));
+            setcookie('role', $role,  time() + (60 * 60 * 24 * 30));
+
+            // Now logged in
+            if ($role == 'ROLE_01') {    // redirecting to correct homepage
+                echo "<script> location.href='b-home.php'; </script>";
+            } elseif ($role == 'ROLE_02') {
+                echo "<script> location.href='s-home.php'; </script>";
+            } elseif ($role == 'ROLE_03') {
+                echo "<script> location.href='./a-home.html'; </script>";
+            } else { echo "<div align='center'>
+               <p><strong>Please enter a valid username and password</strong></p>
+                </div>";
+            }
+
+
+
+        }
+
+
+        ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     </form>
 
@@ -79,74 +184,12 @@
 </html>
 
 
-<?php include 'config.php'; ?>
-<?php
-if(isset($_POST["submit"]))
-{
-
-
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $query = "SELECT * FROM user WHERE EMAIL = '$email' ";
-
-
-    $result = mysqli_query($conn, $query);
-
-    while($row = mysqli_fetch_assoc($result)) {
-
-        $userID = $row['ID_USER'];
-        $db_pass = $row['PASS'];
-        $first_name = $row['FNAME'];
-        $last_name = $row['LNAME'];
-        $email = $row['EMAIL'];
-        $role = $row['ID_ROLE'];
+<style>
+    .jumbotron {
+        background-color: transparent;
     }
 
-    $userID = $userID;
-    $first_name =$first_name;
-    $last_name = $last_name;
-    $role = $role;
-
-    session_start();
-
-
-        // if valid
-        $_SESSION['userID'] = $userID;
-        $_SESSION['username'] = $email;
-        $_SESSION['first_name'] = $first_name;
-        $_SESSION['last_name'] = $last_name;
-        $_SESSION['role'] = $role;
-
-
-        setcookie('userID', $userID,   time() + (60 * 60 * 24 * 30));
-        setcookie('username', $email,  time() + (60 * 60 * 24 * 30));
-        setcookie('first_name', $first_name,  time() + (60 * 60 * 24 * 30));
-        setcookie('last_name', $last_name,  time() + (60 * 60 * 24 * 30));
-        setcookie('role', $role,  time() + (60 * 60 * 24 * 30));
-
-        // Now logged in
-            if ($role == 'ROLE_01') {    // redirecting to correct homepage
-                echo "<script> location.href='b-home.php'; </script>";
-            } elseif ($role == 'ROLE_02') {
-                echo "<script> location.href='s-home.php'; </script>";
-            } elseif ($role == 'ROLE_03') {
-              echo "<script> location.href='./a-home.html'; </script>";
-              } else { echo "<div align='center'>
-               <p><strong>Please enter a valid username and password</strong></p>
-                </div>";
-            }
-
-
-
-}
-
-
-?>
-
-
-
+</style>
 
 
 
