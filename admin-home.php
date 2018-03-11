b6d258566066b065056b381f4adf308e82cd35f6
Wontek <wontekh@gmail.com>
2018-03-11 03:39:47 +0000

    Administrator Full Version

---
+++ admin-home.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anthony
+ * Date: 11/03/2018
+ * Time: 03:15
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Home</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta charset='UTF-8'>
+
+    <style class="cp-pen-styles">body {
+            background-color: #f3f5f7;
+            font-family: 'Helvetica Neue', Arial, sans-serif;
+        }
+
+        .card {
+            background-color: #fff;
+            box-shadow: 0 0 6px rgba(0, 0, 0, 0.1);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            max-width: 300px;
+            height: 375px;
+            border-radius: 10px;
+            overflow: hidden;
+        }
+
+        .card .about {
+            height: 225px;
+            padding: 20px;
+            box-sizing: border-box;
+        }
+
+        .card .about h3,
+        .card .about .lead {
+            font-weight: 300;
+            margin: 0;
+        }
+
+        .card .about h3 {
+            font-size: 24px;
+        }
+
+        .card .info p {
+            font-size: 11px;
+            color: #aaa;
+            font-weight: 300;
+        }
+
+    </style>
+</head>
+
+<!--List of auctions, items, users. Analytics.-->
+
+
+<body>
+
+<div class="card" style="height:auto; width:auto;">
+    <div class="col-sm-12" style="height:auto; width:auto;">
+        <!-- Custom information -->
+        <div class="about" style="height:auto; width:auto;">
+            <img class="card-img-top" src="efast.png" alt="Card image">
+            <div class="spacer" style="height:20px"></div>
+            <h3>Welcome to the Administrator's Home</h3>
+
+            <div class="spacer" style="height:30px"></div>
+            <div class="btn-group-vertical btn-block ">
+                <button type="button" class="btn btn-primary" onclick="window.location='./upload-category.php';">Add new Category</button>
+                <div class="spacer" style="height:5px"></div>
+                <button type="button" class="btn btn-primary" onclick="window.location='./upload-duration.php';">Add new Duration</button>
+                <div class="spacer" style="height:5px"></div>
+                <button type="button" class="btn btn-primary" onclick="window.location='./auctions.php';">Inspect all Auctions</button>
+                <div class="spacer" style="height:5px"></div>
+                <button type="button" class="btn btn-primary" onclick="window.location='./users.php';">Inspect all users</button>
+                <div class="spacer" style="height:30px"></div>
+                <button type="button" class="btn btn-default">Logout</button>
+
+            </div>
+            <div class="spacer" style="height:20px"></div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ auctions.php
@@ -0,0 +1,254 @@
+<?php
+
+session_start();
+require_once("config.php");
+
+?>
+<?php
+
+// ----------------------------------------------------------------------------------------------------
+// - Display Errors
+// ----------------------------------------------------------------------------------------------------
+ini_set('display_errors', 'On');
+ini_set('html_errors', 0);
+
+// ----------------------------------------------------------------------------------------------------
+// - Error Reporting
+// ----------------------------------------------------------------------------------------------------
+error_reporting(-1);
+
+// ----------------------------------------------------------------------------------------------------
+// - Shutdown Handler
+// ----------------------------------------------------------------------------------------------------
+function ShutdownHandler()
+{
+    if(@is_array($error = @error_get_last()))
+    {
+        return(@call_user_func_array('ErrorHandler', $error));
+    };
+
+    return(TRUE);
+};
+
+register_shutdown_function('ShutdownHandler');
+
+// ----------------------------------------------------------------------------------------------------
+// - Error Handler
+// ----------------------------------------------------------------------------------------------------
+function ErrorHandler($type, $message, $file, $line)
+{
+    $_ERRORS = Array(
+        0x0001 => 'E_ERROR',
+        0x0002 => 'E_WARNING',
+        0x0004 => 'E_PARSE',
+        0x0008 => 'E_NOTICE',
+        0x0010 => 'E_CORE_ERROR',
+        0x0020 => 'E_CORE_WARNING',
+        0x0040 => 'E_COMPILE_ERROR',
+        0x0080 => 'E_COMPILE_WARNING',
+        0x0100 => 'E_USER_ERROR',
+        0x0200 => 'E_USER_WARNING',
+        0x0400 => 'E_USER_NOTICE',
+        0x0800 => 'E_STRICT',
+        0x1000 => 'E_RECOVERABLE_ERROR',
+        0x2000 => 'E_DEPRECATED',
+        0x4000 => 'E_USER_DEPRECATED'
+    );
+
+    if(!@is_string($name = @array_search($type, @array_flip($_ERRORS))))
+    {
+        $name = 'E_UNKNOWN';
+    };
+
+    return(print(@sprintf("%s Error in file \xBB%s\xAB at line %d: %s\n", $name, @basename($file), $line, $message)));
+};
+
+$old_error_handler = set_error_handler("ErrorHandler");
+
+// other php code
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Buyer MyProfile</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/profile.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .checked {
+            color: orange;
+        }
+    </style>
+</head>
+
+<body>
+
+<!-- nav bar -->
+
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand">
+        <img src="eFast.png" width="100" height="30" alt="">
+    </a>
+</nav>
+
+
+    <!-- Bid history -->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+
+    <div class="container">
+        <button type="button" class="btn btn-default btn-sm" onclick="window.location='./admin-home.php';"><i class="glyphicon glyphicon-arrow-left"></i> Back to Home</button>
+        <div class="row">
+
+
+            <div class="col-md-12">
+                <h2>All Auctions</h2>
+                <hr/>
+                <div class="table-responsive">
+
+
+                    <table id="mytable" class="table table-bordred table-striped">
+
+                        <thead>
+
+
+                        <th>Item Name</th>
+                        <th>Item Seller</th>
+                        <th>Item Description</th>
+                        <th>Max Bid</th>
+                        <th>Auction Time Remaining</th>
+                        </thead>
+                        <tbody>
+
+                        <?php
+                        $sql11 = "SELECT ID_AUCTION, ID_SELLER, TITLE, DESCRIPTION, EXPIRATION_TIME FROM auction a INNER JOIN item i ON a.ID_ITEM = i.ID_ITEM
+                                      ORDER BY EXPIRATION_TIME DESC";
+                        $result11 = $conn->query($sql11);
+                        if(!$result11) {throw new Exception("Database Error6");}
+                        $row11 = $result11->fetch_assoc();
+                        while($row11 = $result11->fetch_assoc()){
+                            $currentauctionID = $row11['ID_AUCTION'];
+                            $now = date('Y-m-d H:i:s');
+                            $expiration_datetime = $row11["EXPIRATION_TIME"];
+                            $description = $row11['DESCRIPTION'];
+                            $diff=strtotime($expiration_datetime)-strtotime($now);
+
+                            if($diff>0) {
+
+                                //  convert to days
+                                $temp = $diff / 86400; // 86400 secs in a day
+
+                                // days
+                                $days = floor($temp);
+                                $temp = 24 * ($temp - $days);
+                                // hours
+                                $hours = floor($temp);
+                                $temp = 60 * ($temp - $hours);
+                                // minutes
+                                $minutes = floor($temp);
+                                $temp = 60 * ($temp - $minutes);
+                                // seconds
+                                $seconds = floor($temp);
+
+                                if ($days > 0) {
+                                    $timeremaining = "{$days} days {$hours} hours";
+                                } elseif ($hours > 0) {
+                                    $timeremaining = "{$hours} hours {$minutes} minutes";
+                                } elseif ($minutes > 0) {
+                                    $timeremaining = "{$minutes} minutes {$seconds} seconds";
+                                } elseif ($seconds > 0) {
+                                    $timeremaining = "{$seconds} seconds";
+                                }
+                            }
+                            else {$timeremaining = "Auction Complete";}
+
+                        $sql12 = "SELECT MAX(PRICE) as HELLO FROM bid WHERE ID_AUCTION = '$currentauctionID'";
+                        $result12 = $conn->query($sql12);
+                        if(!$result12) {throw new Exception("Database Error7");}
+                        $row12 = $result12->fetch_assoc();
+                        $maxbid = $row12['HELLO'];
+                        if ($maxbid == NULL){
+                            $maxbid = "No bid yet";
+                        }
+
+
+                            ?>
+
+                            <tr>
+                                <td><?php echo $row11["TITLE"]?></td>
+                                <td><?php echo $row11["ID_SELLER"]?></td>
+                                <td><?php echo $description ?></td>
+                                <td><?php echo $maxbid?></td>
+                                <td><?php echo $timeremaining ?></td>
+                            </tr>
+
+                            <div class="modal fade" <?php echo "id='delete".$m."'" ?> tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+                                <php if $
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+                                            <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this bid?</div>
+
+                                        </div>
+                                        <div class="modal-footer ">
+                                            <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
+                                            <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
+                                        </div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div>
+                        <?php }  ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- This section is the delete auction confirmation - but this isn't in the required features -->
+
+    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+        <php if $
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+                    <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
+                </div>
+                <div class="modal-body">
+
+                    <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this bid?</div>
+
+                </div>
+                <div class="modal-footer ">
+                    <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+
+
+</body>
+
+</html>
+
+
